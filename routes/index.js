932bc290c22d65220ae0ccdd585de2f52dbb7241
Ridokunda <ridokundanetshi@gmail.com>
2024-04-05 17:06:01 +0200

    added routes to register and log in page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+// GET registerCustomer page
+router.get('/registerCustomer', function(req, res, next) {
+  res.render('registerCustomer', { title: 'Register Page' });
+});
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+  res.render('login',{ title: 'Log in'});
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
--- routes/registerCustomer.js
+++ routes/registerCustomer.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 //get register customer page
-router.get('/registerCustomer', function(req,res,next) {
-    res.render('registerCustomer');
+router.get('/registerCustomer', function(req, res, next) {
+    res.render('registerCustomer', { title: 'Register Page' });
 });
 
 module.exports = router;
