fa96254e7004d27099d426f954dcd3aae2069268
Ridokunda <ridokundanetshi@gmail.com>
2024-06-22 18:26:26 +0200

    worked on booking

--- routes/booking.js
+++ routes/booking.js
@@ -5,12 +5,17 @@ const connection = require("../database/connection");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if(!req.session.userid){
+    return res.redirect('/login');
+  }
   res.render('booking', { title: 'Book a Trip' });
 });
 
 router.post('/', (req,res, next) => {
-    
-    
+  if(!req.session.userid){
+    return res.redirect('/login');
+  }
+   
   var service = req.body.service;
   var date_start = req.body.date_start;
   var des = req.body.description;
@@ -19,10 +24,13 @@ router.post('/', (req,res, next) => {
     
   const query = 'INSERT INTO bookings (idUser,type,date_start,des) VALUES(?,?,?,?)';
 
-  connection.query(query, [3,service,date_start,des], (err, result) =>{
-    if(err) throw err;
+  connection.query(query, [req.session.userid,service,date_start,des], (err, result) =>{
+    if(err){
+      console.error('Error insert booking in the database', err);
+      return res.status(500).send('Internal server error');
+    }
     console.log('booking added');
-    res.redirect('/booking');
+    res.send('Booking added');
   })
 });
 
