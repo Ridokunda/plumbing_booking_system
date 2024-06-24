2ee1282296207199c7754f9b2c618f1a22c6fa56
Ridokunda <ridokundanetshi@gmail.com>
2024-06-24 17:31:26 +0200

    created ui for bookings display for admin

--- routes/bookings.js
+++ routes/bookings.js
@@ -3,7 +3,19 @@ var router = express.Router();
 const connection = require('../database/connection');
 
 router.get('/', function(req,res,next){
-    res.render('bookings', {title:'Bookings'});
-})
+    
+    res.setHeader('Cache-Control', 'no-store');
+    connection.query('SELECT * FROM bookings', (err, results) => {
+        if (err) {
+            console.error('Error executing MySQL query: ' + err.stack);
+            res.status(500).send('Error fetching bookings');
+            return;
+        }
+        console.log('bookings found');
+        res.render('bookings', { bookings: results , title : 'Bookings' });
+    });
+    
+    
+});
 
 module.exports = router;
